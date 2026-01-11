c80822237a95e2c35a80ac357906e172f202ffe1
kopk8 <joseantonionieli25@hotmail.com>
2026-01-11 18:31:20 +0100

    inserite le variabili

--- index.php
+++ index.php
@@ -10,13 +10,3 @@ var_dump($string);
 var_dump($bool);
 
 
-// traccia costanti
-const INTERO = 25; 
-const NONINTERO = 85.5;
-const STRINGA = "Stringa";
-const BOOLEANO = true;
-
-var_dump(INTERO);
-var_dump(NONINTERO);
-var_dump(STRINGA);
-var_dump(BOOLEANO);
