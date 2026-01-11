9fa56749ac14c8a6c7bb1d1c1c4937347bec2c07
kopk8 <joseantonionieli25@hotmail.com>
2026-01-11 18:28:22 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+$integer = 25; 
+$float = 85.5;
+$string = "Stringa";
+$bool = true;
+
+var_dump($integer);
+var_dump($float);
+var_dump($string);
+var_dump($bool);
+
+
+// traccia costanti
+const INTERO = 25; 
+const NONINTERO = 85.5;
+const STRINGA = "Stringa";
+const BOOLEANO = true;
+
+var_dump(INTERO);
+var_dump(NONINTERO);
+var_dump(STRINGA);
+var_dump(BOOLEANO);
