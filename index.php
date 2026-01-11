76dd70b1390a011e817c2d9098088ab4855fedad
kopk8 <joseantonionieli25@hotmail.com>
2026-01-11 18:32:46 +0100

    modificate le variabili con le costanti

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <?php
-$integer = 25; 
-$float = 85.5;
-$string = "Stringa";
-$bool = true;
+const INTERO = 25; 
+const NONINTERO = 85.5;
+const STRINGA = "Stringa";
+const BOOLEANO = true;
 
-var_dump($integer);
-var_dump($float);
-var_dump($string);
-var_dump($bool);
-
+var_dump(INTERO);
+var_dump(NONINTERO);
+var_dump(STRINGA);
+var_dump(BOOLEANO);
 
